8451d0117438ac58e21cc58f938b95b6486f4681
RodrigoAnciaes <81170574+RodrigoAnciaes@users.noreply.github.com>
2024-09-20 13:07:55 -0300

    Some fixes.

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from datetime import date
+from datetime import date, datetime
 import dev_abertoV2
 import babel.dates
 import gettext
@@ -10,6 +10,8 @@ if __name__ == '__main__':
     _ = gettext.gettext
     try:
         date, name = dev_abertoV2.hello()
+        date = datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
     except:
         date, name = date.today(), 'Desconhecido'
+        
     print(_('Último commit feito em: {} por {}').format(babel.dates.format_date(date), name))
